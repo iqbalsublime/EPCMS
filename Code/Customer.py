d6f5a1289a0aa981a1a0a1a98d75edbe3ef5a037
Md. Iqbal Hossain <iqbal.sublime@gmail.com>
2019-11-10 23:43:05 +0600

    initial commit for Computer Programming project

---
+++ Code/Customer.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 10 22:16:24 2019
+
+@author: ASUS
+"""
+
+class Customer:
+
+
+    # Initializer / Instance Attributes
+    def __init__(self, cid, name, mobile):
+        self.cid = cid
+        self.name = name
+        self.mobile = mobile
+
+
+    def getCustomerName(self):
+        return self.name
+    
+    # instance method
+    def description(self):
+        return "{}'s Mobile: {}".format(self.name, self.mobile)
+
+cust1= Customer(1,"Iqbal", "01671998671")
+#print(cust1.description())
