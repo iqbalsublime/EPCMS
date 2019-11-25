4d72fc08d5fb8e3202ec01d165a00835008cfd50
Md. Iqbal Hossain <iqbal.sublime@gmail.com>
2019-11-26 01:49:17 +0600

    invoice updated

--- Code/Menu.py
+++ Code/Menu.py
@@ -9,14 +9,15 @@ Created on Wed Nov 20 02:24:12 2019
 class Menu:
     CAT = ('Fast Food', 'Bangla', 'Indian', 'Misc')
     # Initializer / Instance Attributes
-    def __init__(self, mid, name, price, category):
+    def __init__(self, mid, name, price, category, quantity):
         if category not in self.CAT:
             raise ValueError("%s is not a valid category." % category)
         self.mid = mid
         self.name = name
         self.price = price
         self.category = category
+        self.quantity = quantity
 
 
-menu1= Menu(1,"Burger", 160,"Fast Food")
+menu1= Menu(1,"Burger", 160,"Fast Food",2)
 #print(rest1.description())
